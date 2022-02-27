b7ed71d0d932e6fb731119e1b960f120f38a6f2c
Jerke Combee <jerke@elgentos.nl>
2022-02-27 11:41:32 +0100

    Chamge database field types

--- database/migrations/2022_02_25_163134_create_references_table.php
+++ database/migrations/2022_02_25_163134_create_references_table.php
@@ -16,7 +16,7 @@ return new class extends Migration {
             $table->id();
 
             $table->string('type');
-            $table->json('value');
+            $table->text('value');
             $table->morphs('referenceable');
 
             $table->timestamps();
